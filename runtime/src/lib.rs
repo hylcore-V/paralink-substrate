5a8336a95aa505652e557a37f8005c0ccd167d6b
hylcore-V <hyliver0109@gmail.com>
2020-12-14 02:24:59 +0100

    Rename node/runtime

--- runtime/src/lib.rs
+++ runtime/src/lib.rs
@@ -92,8 +92,8 @@ pub mod opaque {
 }
 
 pub const VERSION: RuntimeVersion = RuntimeVersion {
-	spec_name: create_runtime_str!("node-template"),
-	impl_name: create_runtime_str!("node-template"),
+	spec_name: create_runtime_str!("paralink-network"),
+	impl_name: create_runtime_str!("paralink-network"),
 	authoring_version: 1,
 	spec_version: 1,
 	impl_version: 1,
